8c0f9bb491004c862f3873323218e795f7912faa
DirkEves <dirk.eveslage@hs-osnabrueck.de>
2022-12-07 22:28:06 +0100

    JPA Associations @RequestScoped und @Transactional haben Fehler aufgeworfen. Daher auskommentiert. Event hat eine Collection mit Usern die als Beziehungstabelle in SQL dargestellt wird.

--- src/main/java/Eventverwaltung/Event/entity/internal/Event.java
+++ src/main/java/Eventverwaltung/Event/entity/internal/Event.java
@@ -1,10 +1,12 @@
 package Eventverwaltung.Event.entity.internal;
 
 import Eventverwaltung.Event.entity.EventTO;
+import Eventverwaltung.Teilnehmer.entity.User;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Collection;
 
 @Entity
 @Table(name = "wichtel_event")
@@ -17,13 +19,17 @@ public class Event implements Serializable {
     private String regeln;
     private LocalDateTime deadline;
     private LocalDateTime eventDate;
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(joinColumns = @JoinColumn(name = "Event_EventId", referencedColumnName = "eventId"))
+    private Collection<User> mitglieder;
 
-    public Event(int eventId, String name, String regeln, LocalDateTime deadline, LocalDateTime eventDate) {
+    public Event(int eventId, String name, String regeln, LocalDateTime deadline, LocalDateTime eventDate, Collection<User> mitglieder) {
         EventId = eventId;
         this.name = name;
         this.regeln = regeln;
         this.deadline = deadline;
         this.eventDate = eventDate;
+        this.mitglieder = mitglieder;
     }
 
     public Event(){
@@ -79,5 +85,13 @@ public class Event implements Serializable {
 
     public void setEventDate(LocalDateTime eventDate) {
         this.eventDate = eventDate;
+    }
+
+    public Collection<User> getMitglieder() {
+        return mitglieder;
+    }
+
+    public void setMitglieder(Collection<User> mitglieder) {
+        this.mitglieder = mitglieder;
     }
 }
