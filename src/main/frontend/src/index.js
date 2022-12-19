abec49ef80f41ae2bf9bb896ea6ac784df06e891
pgs3238 <114508458+pgs3238@users.noreply.github.com>
2022-12-19 14:05:49 +0100

    Frontend React-JSX Added

---
+++ src/main/frontend/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+//import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+function Welcome(props) {
+    return <h1> Hello, {props.name}, {props.nachname}</h1>
+}
+
+const element = <Welcome name="Sara" nachname="Simpson" />;
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+/*const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);*/
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
