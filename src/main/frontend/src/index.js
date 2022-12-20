6efc0856b4f19aff54865735a6acd6f135ecdc9d
pgs3238 <114508458+pgs3238@users.noreply.github.com>
2022-12-20 13:16:23 +0100

    old to new

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-//import ReactDOM from 'react-dom/client';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+//alter code, darueber neuer code
+//import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-function Welcome(props) {
+//alter code - darunter neuer code
+/*function Welcome(props) {
     return <h1> Hello, {props.name}, {props.nachname}</h1>
 }
 
@@ -15,17 +17,17 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));*/
 
 
 
 
-/*const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);*/
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
