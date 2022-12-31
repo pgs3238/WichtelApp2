e31240193513d60fc25ba168a78249afae20a4ff
pgs3238 <114508458+pgs3238@users.noreply.github.com>
2022-12-31 10:45:48 +0100

    added notation

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //alter code, darueber neuer code
 //import ReactDOM from 'react-dom';
+//folgende 3 Zeilen machen das drehende Bild. In Zeile 44 muss dazu Apps zu App
 /*import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';*/
@@ -47,6 +48,7 @@ root.render(
  /* If you want to start measuring performance in your app, pass a function
  to log results (for example: reportWebVitals(console.log))
  or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals */
+//TODO not implemented yet (???) so deaktivated currently 31.12.2022
 // reportWebVitals();
 
 //Create Routes (Seiten verknüpfen aus pages)
