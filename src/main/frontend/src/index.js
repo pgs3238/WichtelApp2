0b9e7e5637b9e563d5582fabba5574af8e7604a5
pgs3238 <114508458+pgs3238@users.noreply.github.com>
2022-12-31 10:41:39 +0100

    Fehler behoben, Seite getestet. Funktioniert.

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //alter code, darueber neuer code
 //import ReactDOM from 'react-dom';
-import './index.css';
+/*import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';*/
 
 //Routes Import - 27.12.22 Schnieders
 //import ReactDOM from "react-dom";
@@ -40,14 +40,14 @@ ReactDOM.render(<App />, document.getElementById('root'));*/
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Apps />
   </React.StrictMode>
 );
 
  /* If you want to start measuring performance in your app, pass a function
  to log results (for example: reportWebVitals(console.log))
  or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals */
-reportWebVitals();
+// reportWebVitals();
 
 //Create Routes (Seiten verknüpfen aus pages)
 export default function Apps() {
@@ -76,4 +76,6 @@ export default function Apps() {
     );
 }
 
-ReactDOM.render(<Apps />, document.getElementById("root"));
+
+// Problem - ähnliche (alte Anweisung) wie Zeile 40-45. Zeile muss abgeändert werden zum Format von 40-45 um fehler zu beheben.
+// ReactDOM.render(<Apps />, document.getElementById("root"));
