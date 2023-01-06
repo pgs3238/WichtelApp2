b10fde156ea2deadb2470aebf201cea41f848feb
arditv9 <arditv.av@gmail.com>
2023-01-06 22:03:28 +0100

    test ardit

--- src/main/frontend/src/pages/EventAnlegen.jsx
+++ src/main/frontend/src/pages/EventAnlegen.jsx
@@ -1,9 +1,71 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 
 function Layout() {
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const eventName = event.target.eventName;
+        const value = event.target.value;
+        setInputs(values => ({...values, [eventName]: value}))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(inputs);
+    }
+
+    const [date, setDate] = useState();
+
     return (
-        <h1>Neues Event anlegen</h1>
+        <form onSubmit={handleSubmit}>
+            <h1>Secret Santa</h1>
+            <h2>create a new event:</h2>
+            <label>Event name:
+                &emsp;&emsp;&nbsp;
+                <input
+                    type = "text"
+                    id = "ename"
+                    name = "eventName"
+                    value = {inputs.eventName || ""}
+                    onChange = {handleChange}
+                />
+            </label>
+            <br/>
+
+            <label>Event date:
+                <input
+                    type = "date"
+                    onChange = {e=>setDate(e.target.value)}
+                />
+                <p>Selected Date: {date}</p>
+            </label>
+            <br/>
+
+            <label>Event location:
+                <input
+                    type="text"
+                    id = "location"
+                    value = {inputs.eventLocation || ""}
+                    onChange = {handleChange}
+                    />
+            </label>
+            <br/>
+
+            <label>Secret Santa distribution:
+                <input
+                    type = "date"
+                    onChange = {e=>setDate(e.target.value)}
+                />
+                <p>Selected Date: {date}</p>
+            </label>
+            <br/>
+
+            <button>cancel</button>
+
+            <button>save event</button>
+
+        </form>
     );
 }
 
