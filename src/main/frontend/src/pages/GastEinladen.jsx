cfb91e68eecd6e84b40e3119b9449651a355eb6d
Rene169 <94362463+Rene169@users.noreply.github.com>
2023-01-08 22:07:11 +0100

    Frontends angepasst für die meisten Seiten

--- src/main/frontend/src/pages/GastEinladen.jsx
+++ src/main/frontend/src/pages/GastEinladen.jsx
@@ -1,9 +1,63 @@
+import React, {useState} from "react";
 
-import React from 'react';
+function Layout () {
 
-function Layout() {
+    const [inputs, setInputs] = useState ({});
+
+    const handleSubmit = (event) => {
+        event.preventDefault ();
+        alert (inputs);
+    }
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs (values => ({...values, [name]: value}))
+    }
     return (
-        <h1>Gast Einladen</h1>
+        <form onSubmit={handleSubmit}>
+            <h1>GastEinladen</h1>
+
+            <label>E-Mail:
+                &emsp;&emsp;
+            </label>
+
+            <br/>
+
+
+            <input
+                type = "text"
+                placeholder= ""
+                id = "email"
+                name = "email"
+                value = {inputs.email || ""}
+                onChange = {handleChange}
+            />
+
+            <br/><br/>
+
+            <input
+                type="submit"
+                id="emailZuListe"
+                value="E-Mail zu Liste"/>
+
+            <br/>
+            <br/>
+
+            <input
+                type="submit"
+                id="zurueck"
+                value="Zurueck"/>
+            &emsp;&emsp;
+
+            <input
+                type="submit"
+                id="listeEinladen"
+                value="Liste einladen"/>
+
+
+        </form>
+
     );
 }
 
