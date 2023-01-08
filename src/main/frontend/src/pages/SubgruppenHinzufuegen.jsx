857c2b97c65218728219c0222722456217f03006
arditv9 <arditv.av@gmail.com>
2023-01-08 15:52:11 +0100

    Subgruppen Masken mehr oder weniger erstellt.

--- src/main/frontend/src/pages/SubgruppenHinzufuegen.jsx
+++ src/main/frontend/src/pages/SubgruppenHinzufuegen.jsx
@@ -1,9 +1,60 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 
 function Layout() {
+
+    const [inputs, setInputs] = useState({});
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(inputs);
+    }
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+
     return (
-        <h1>Subgruppen hinzufügen</h1>
+        <form onSubmit={handleSubmit}>
+            <h1>Subgruppen hinzufügen</h1>
+
+            <input
+                type = "text"
+                placeholder= "neue Subgruppe"
+                id = "sgruppe"
+                name = "sgruppe"
+                value = {inputs.sgruppe || ""}
+                onChange = {handleChange}
+            />
+
+
+            <br/>
+            <br/>
+            <br/>
+
+
+
+
+
+            <input
+                type="submit"
+                id="hinzufuegen"
+                value = "Hinzufügen"/>
+            &emsp;&emsp;
+
+
+            <input
+                type="submit"
+                id="Abbrechen"
+                value="Abbrechen"/>
+
+
+
+
+            </form>
+
     );
 }
 
--- src/main/frontend/src/pages/UserZuSubgruppeHinzufuegen.jsx
+++ src/main/frontend/src/pages/UserZuSubgruppeHinzufuegen.jsx
@@ -1,10 +1,52 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 
 function Layout() {
-    return (
-        <h1>User zu Subgruppe</h1>
-    );
-}
 
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(inputs);
+    }
+
+        return (
+            <form onSubmit={handleSubmit}>
+                <h1>User zu Subgruppen hinzufügen</h1>
+
+                <br/>
+
+                <th>[gewählten User] hinzufügen zu</th>
+
+                <br/>
+
+                <input list = "data"/>
+                <datalist id = "data">
+                    <option>Mustermann</option>
+                    <option>A Team</option>
+
+                </datalist>
+
+
+                <br/>
+                <br/>
+
+
+
+                <button type="submit">Zu Subgruppe hinzufügen</button>
+
+                <br/>
+                <br/>
+
+                <button type="submit">zurück</button>
+            </form>
+        );
+
+}
 export default Layout;
