a869979b1aa849a34fe940d3d5e636c9c874588f
DirkEves <dirk.eveslage@hs-osnabrueck.de>
2023-01-20 14:15:59 +0100

    Wichtelzuordnung Überprüfungen angelegt

--- src/main/java/Eventverwaltung/Teilnehmer/usecase/impl/Wichtelzuordnung.java
+++ src/main/java/Eventverwaltung/Teilnehmer/usecase/impl/Wichtelzuordnung.java
@@ -20,20 +20,29 @@ public class Wichtelzuordnung implements IWichtelzuordnung{
         Collection<User_Event> wichtelListe = user_eventDAO.findUserEventByEvent(event);
         Map<UserTO, UserTO> zuordnung = new HashMap<>();
 
+        //prüft ob das Event schon durchgelost wurde
         if (istSchonDurchgelost(teilnehmerByEvent)){
             return null;
         }
-        //prüft ob sich mehr als eine Person in dem Event befinden
-        if(teilnehmerByEvent.size()>1) {
 
-            for (User_Event ue : teilnehmerByEvent) {
-                Optional<User_Event> neuWichtel = teilnehmerByEvent.stream().skip(new Random()
-                        .nextInt(teilnehmerByEvent.size())).findFirst();
-                if (neuWichtel.get().equals(ue) || neuWichtel.get().getSubgruppe().equals(ue.getSubgruppe())) {
-
-                }
-            }
+        //prüft ob die Subgruppen alle unter 50% der Teilnehmer haben
+        if(!kleineSubgruppen(event)){
+            return null;
         }
+
+        //prüft ob sich mehr als eine Person in dem Event befinden
+        if(teilnehmerByEvent.size() == 1) {
+            return null;
+        }
+
+        for (User_Event ue : teilnehmerByEvent) {
+             Optional<User_Event> neuWichtel = teilnehmerByEvent.stream().skip(new Random()
+                     .nextInt(teilnehmerByEvent.size())).findFirst();
+             if (neuWichtel.get().equals(ue) || neuWichtel.get().getSubgruppe().equals(ue.getSubgruppe())) {
+
+             }
+        }
+
             return null;
     }
 
@@ -54,7 +63,23 @@ public class Wichtelzuordnung implements IWichtelzuordnung{
     }
 
     //prüft ob keine der Subgruppen mehr als 50% der Teilnehmer hat
-    public boolean kleineSubgruppen(Collection<User_Event> liste){
+    public boolean kleineSubgruppen(EventTO event){
+        Collection<User_Event> subgrListe = user_eventDAO.getSubgruppeNachGroesse(event);
+        Collection<User_Event> alleTeilnehmer = user_eventDAO.findUserEventByEvent(event);
+
+        //ermittelt die gesamtgroesse an Teilnehmern des Events
+        int gesamtgroesse =0;
+        for(User_Event ue : alleTeilnehmer){
+                gesamtgroesse++;
+        }
+
+        //vergleicht ob jeder Wert aus dem SubgruppenCount größer als 50% ist
+        for(User_Event ue : subgrListe){
+            int wert = Integer.parseInt(subgrListe.toString());
+            if (wert > gesamtgroesse*0.5){
+                return false;
+            }else continue;
+        }
      return true;
     }
 }
