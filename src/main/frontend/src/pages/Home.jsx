965e70c73324f0706912aa0d0c343a5edfea2e21
pgs3238 <114508458+pgs3238@users.noreply.github.com>
2023-01-20 15:31:59 +0100

    EventAnlegen...

--- src/main/frontend/src/pages/Home.jsx
+++ src/main/frontend/src/pages/Home.jsx
@@ -12,7 +12,7 @@ function Layout() {
 
             <h3>Hier hast du die Möglichkeit dich zusammen mit Familie, Freunden oder
                 Kollegen für eine unterhaltsame Wichtelrunde anzumelden. Sobald die Wichtelrunde durch den Organisator
-                gestartet wird, wirst du per Zufall eine Person zugewiesen bekommen, für die du ein Geschenk kaufen kannst.
+                gestartet wird, wirst du per Zufall einer Person zugewiesen werden, für die du ein Geschenk kaufen kannst.
                 Lass die Vorfreude beginnen und melde dich jetzt an.
             </h3>
 
