88e26a04eb677772b5355651ceba4653c4824ffd
pgs3238 <114508458+pgs3238@users.noreply.github.com>
2025-11-14 17:04:37 +0100

    have a bug in intellij - might be due to my old Version. If I don't update my offline-development branch and change to Master, Project View and gradlew quarkusDev run offline-development branch even if Master branch is checked out. Updating Intellij, got to test this further.

--- src/main/java/Eventverwaltung/Teilnehmer/entity/internal/UserEvent.java
+++ src/main/java/Eventverwaltung/Teilnehmer/entity/internal/UserEvent.java
@@ -3,6 +3,8 @@ package Eventverwaltung.Teilnehmer.entity.internal;
 import Eventverwaltung.Event.entity.internal.Event;
 
 import javax.persistence.*;
+import java.util.Set;
+import java.util.HashSet;
 
 @Entity
 @Table(name="wichtel_event_wichtel_user1")
@@ -28,15 +30,29 @@ public class UserEvent {
     @Column(name = "radio", nullable = false)
     private int radio = 0; //default value 0
 
+    @ElementCollection
+    @CollectionTable(
+            name = "user_event_exclusions",
+            joinColumns = {
+                    @JoinColumn(name = "user_email", referencedColumnName = "usermail"),
+                    @JoinColumn(name = "event_id", referencedColumnName = "event_eventid")
+            }
+    )
+    @Column(name = "excluded_email")
+    private Set<String> excludedEmails = new HashSet<>();
 
-    public UserEvent(UserEventId id, User user, Event event, int radio) {
+
+    public UserEvent(UserEventId id, User user, Event event, int radio, Set<String> excludedEmails) {
         this.id = id;
         this.user = user;
         this.event = event;
         this.radio = radio;
+        this.excludedEmails = excludedEmails != null ? excludedEmails : new HashSet<>();
     }
 
-    public UserEvent() {}
+    public UserEvent() {
+        this.excludedEmails = new HashSet<>();
+    }
 
     public UserEventId getId() {
         return id;
@@ -68,5 +84,13 @@ public class UserEvent {
 
     public void setRadio(int radio) {
         this.radio = radio;
+    }
+
+    public Set<String> getExcludedEmails() {
+        return excludedEmails;
+    }
+
+    public void setExcludedEmails(Set<String> excludedEmails) {
+        this.excludedEmails = excludedEmails != null ? excludedEmails : new HashSet<>();
     }
 }
